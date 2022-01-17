31b928028f8e5eeba1e1655343fabe694d373b04
Aviv Cohn <ACohn@DGITech.com>
2022-01-17 14:26:29 -0500

    Creates a zip file of original file

--- Certified Module to JSON/Program.cs
+++ Certified Module to JSON/Program.cs
@@ -13,15 +13,6 @@ namespace Certified_Module_to_JSON
 
             string new_file = Path.ChangeExtension(File_Location, ".zip");
 
-
-            Console.WriteLine(File_Location);
-            Console.WriteLine(File_Location.Trim());
-            Console.WriteLine(new_file);    
-            Console.WriteLine(new_file.Trim());
-
-
-            //File.Move(File_Location, new_file);
-
             File.Copy(File_Location.Trim('"'), new_file.Trim('"'));
         }
     }
