2a156a4d69776f593a6513c0d88e05942904e6e8
Aviv Cohn <ACohn@DGITech.com>
2022-01-17 17:01:57 -0500

    Mostly Stable

--- Certified Module to JSON/Program.cs
+++ Certified Module to JSON/Program.cs
@@ -1,46 +1,54 @@
 ﻿using System;
 using System.IO;
 using System.IO.Compression;
+
 //using Newtonsoft.Json;
- 
 
 namespace Certified_Module_to_JSON
 {
-    internal class Program
+     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             Console.WriteLine("Add pkg file");
             string File_Location = Console.ReadLine().Trim('"');
 
-            string new_file = Path.ChangeExtension(File_Location, ".zip").Trim('"');
+            string Zip_File = Path.ChangeExtension(File_Location, ".zip").Trim('"');
 
-            File.Copy(File_Location, new_file);
+            File.Copy(File_Location, Zip_File);
 
-            ZipFile.ExtractToDirectory(new_file, Path.GetDirectoryName(new_file));
+            ZipFile.ExtractToDirectory(Zip_File, Path.GetDirectoryName(Zip_File));
 
             byte[] bytes = System.IO.File.ReadAllBytes(Path.ChangeExtension(File_Location, ".dll").Trim('"'));
             string text = System.Text.Encoding.UTF8.GetString(bytes);
 
+            File.Delete(Zip_File);
+            File.Delete(Path.ChangeExtension(Path.GetFullPath(File_Location), ".dll"));
+            File.Delete(Path.ChangeExtension(Path.GetFullPath(File_Location), ".dat"));
+
             //Console.WriteLine(text);
 
-            string Protocol;
+            string Protocol = "";
 
             int From = text.IndexOf(@"{""CrestronSerialDeviceApi""");
-
             int To = text.IndexOf("}}]}}");
 
-            //int To = text.IndexOf("\x02\x00\x00\x00");
+            if (From <= 0)
 
-            Protocol = text.Substring(From, To-From+6);
+            {
+                From = text.IndexOf("{");
+                To = text.IndexOf("BSJB");
+                Protocol = text.Substring(From, To - 1);
+            }
+            else
+            {
+                
+                Protocol = text.Substring(From, To - From + 6);
+            }
 
-            //Console.WriteLine(Path.GetDirectoryName(new_file));
+            File.WriteAllText(Path.ChangeExtension(Path.GetFullPath(File_Location), "_Protocol.json"), Protocol);
 
-            File.WriteAllText(Path.ChangeExtension(Path.GetFullPath(File_Location), ".json"), Protocol);
-            
-
-            //Console.WriteLine(test);
-
+            //Console.WriteLine(Protocol);
         }
     }
 }
