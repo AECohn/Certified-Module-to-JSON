f81f3932f84ab7028731af9e70199a061a47840e
Aviv Cohn <ACohn@DGITech.com>
2022-01-17 20:39:22 -0500

    Adding yallie's JSON formatter

--- Certified Module to JSON/Program.cs
+++ Certified Module to JSON/Program.cs
@@ -1,11 +1,14 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.IO;
 using System.IO.Compression;
+using System.Linq;
 
 namespace Certified_Module_to_JSON
 {
     internal class Program
     {
+    
         private static void Main(string[] args)
         {
             while (true)
@@ -36,26 +39,56 @@ namespace Certified_Module_to_JSON
 
                 if (!File.Exists(json))
                 {
-                    string Protocol;
-
-                    int From = text.IndexOf(@"{""CrestronSerialDeviceApi""");
-                    int To = text.IndexOf("}}]}}");
-
-                    if (From <= 0)
-
+                    try
                     {
-                        From = text.IndexOf("{");
-                        To = text.IndexOf("BSJB");
-                        Protocol = text.Substring(From, To - From - 3);
+                        string Protocol;
+
+                        int From = text.IndexOf(@"{""CrestronSerialDeviceApi""");
+                        int To = text.IndexOf("}}]}}");
+
+                        if (From <= 0)
+
+                        {
+                            From = text.IndexOf("{");
+                            To = text.IndexOf("BSJB");
+                            Protocol = text.Substring(From, To - From - 3);
+                        }
+                        else
+                        {
+                            Protocol = text.Substring(From, To - From + 5);
+                        }
+                        Console.WriteLine("Json extracted from dll");
+                        File.WriteAllText(json, FormatJson(Protocol));
                     }
-                    else
-                    {
-                        Protocol = text.Substring(From, To - From + 5);
+                    catch (Exception ex)
+                    { 
+                        Console.WriteLine(ex.Message); 
                     }
-                    Console.WriteLine("Json extracted from dll");
-                    File.WriteAllText(json, Protocol);
                 }
             }
+        }
+
+        public static string FormatJson(string json, string indent = "  ") //Created by yallie (https://stackoverflow.com/a/57100143)
+        {
+            var indentation = 0;
+            var quoteCount = 0;
+            var escapeCount = 0;
+
+            var result =
+                from ch in json ?? string.Empty
+                let escaped = (ch == '\\' ? escapeCount++ : escapeCount > 0 ? escapeCount-- : escapeCount) > 0
+                let quotes = ch == '"' && !escaped ? quoteCount++ : quoteCount
+                let unquoted = quotes % 2 == 0
+                let colon = ch == ':' && unquoted ? ": " : null
+                let nospace = char.IsWhiteSpace(ch) && unquoted ? string.Empty : null
+                let lineBreak = ch == ',' && unquoted ? ch + Environment.NewLine + string.Concat(Enumerable.Repeat(indent, indentation)) : null
+                let openChar = (ch == '{' || ch == '[') && unquoted ? ch + Environment.NewLine + string.Concat(Enumerable.Repeat(indent, ++indentation)) : ch.ToString()
+                let closeChar = (ch == '}' || ch == ']') && unquoted ? Environment.NewLine + string.Concat(Enumerable.Repeat(indent, --indentation)) + ch : ch.ToString()
+                select colon ?? nospace ?? lineBreak ?? (
+                    openChar.Length > 1 ? openChar : closeChar
+                );
+
+            return string.Concat(result);
         }
     }
 }
